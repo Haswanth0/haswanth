f84e2e61e0d0223d22dc1cf866d2fd56d7edaa70
Haswanth0 <53077667+Haswanth0@users.noreply.github.com>
2019-07-19 14:32:07 +0530

    Update first.py

--- first.py
+++ first.py
@@ -1,7 +1,8 @@
+#gangarajuhaswanth123@gmail.com
 a=int(input())
 if (a==0):
     print('zero')
 elif (a>0):
-    print('positive')
+    print('Positive')
 else :
-    print('negative')
+    print('Negative')
