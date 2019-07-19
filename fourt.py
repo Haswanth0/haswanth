6b7d84c763d2bb399da082bcf11f4b815fc15ff9
Haswanth0 <53077667+Haswanth0@users.noreply.github.com>
2019-07-19 15:47:31 +0530

    Update fourt.py

--- fourt.py
+++ fourt.py
@@ -1,1 +1,7 @@
+#gangarajuhaswanth123@gmail.com
+ch=input()
+if (ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
+    print ('Vowels')
+else:
+     print ('Consonant')
 
